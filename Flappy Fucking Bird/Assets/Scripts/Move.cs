06b65efa8e718b952cf6b9ab5c9d286a2511bdcd
Mustafa Enes Araç <menesarac2002@gmail.com>
2022-07-26 18:11:02 +0300

    Flappy Bird with C# on Unity

---
+++ Flappy Fucking Bird/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed ;
+
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "ScoreArea")
+        {
+
+
+        }
+    }
+}
